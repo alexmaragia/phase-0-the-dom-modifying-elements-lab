bf0af46ff062109aad84494a82e1ac1a7ff28fa0
alexmaragia <maragialex@gmail.com>
2024-07-13 22:24:06 +0300

    DOM Modifying Elements

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your code here!
+// Remove the main element with id 'main'
+const mainElement = document.getElementById('main');
+mainElement.remove();
+
+// Create a new h1 element
+const newHeader = document.createElement('h1');
+
+// h1 element an id of 'victory'
+newHeader.id = 'victory';
+
+// Set the text content of the h1 element
+newHeader.textContent = 'ALEXANDER is the champion';
+
+// Append the h1 element to the body
+document.body.append(newHeader);
